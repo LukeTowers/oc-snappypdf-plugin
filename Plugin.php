da483f50bfad083f466fec3cbe57369f3d4c748e
Luke Towers <luke@luketowers.ca>
2017-04-25 09:28:18 -0600

    Whitespace changes and plugin homepage

--- Plugin.php
+++ Plugin.php
@@ -21,7 +21,8 @@ class Plugin extends PluginBase
             'name'        => 'luketowers.snappypdf::lang.plugin.name',
             'description' => 'luketowers.snappypdf::lang.plugin.description',
             'author'      => 'Luke Towers',
-            'icon'        => 'icon-file-pdf-o'
+            'icon'        => 'icon-file-pdf-o',
+            'homepage'    => 'https://github.com/LukeTowers/oc-snappypdf-plugin',
         ];
     }
     
@@ -78,80 +79,80 @@ class Plugin extends PluginBase
     }
     
     /**
-	 * Checks the configuration for the plugin
-	 */
-	public function validateConfig()
-	{
-		$checkComponents = [
-			'pdf' => [
-				'binary_x64' => 'wkhtmltopdf-amd64',
-				'binary_x32' => 'wkhtmltopdf-i386',
-			],
-			'image' => [
-				'binary_x64' => 'wkhtmltoimage-amd64',
-				'binary_x32' => 'wkhtmltoimage-i386',
-			]
-		];
-		
-		foreach ($checkComponents as $component => $options) {
-			if (!Config::get("snappy.$component.enabled")) {
-				continue;
-			}
-			
-			if (empty(env('SNAPPY_' . strtoupper($component) . '_BINARY'))) {
-				$binaryPathConfigKey = 'snappy.' . $component . '.binary';
-				$binaryBasename = pathinfo(Config::get($binaryPathConfigKey), PATHINFO_BASENAME);
-				if (!file_exists(Config::get($binaryPathConfigKey))) {
-					Config::set($binaryPathConfigKey, $this->getBinaryPath($binaryBasename));
-				}
-				
-				if (!$this->is64Bit() && ($binaryBasename === $options['binary_x64'])) {
-					Config::set($binaryPathConfigKey, $this->getBinaryPath($options['binary_x32']));
-				}
-			}
-			
-			$executable = Config::get($binaryPathConfigKey); 
-			if (!is_executable($executable)) {
-				throw new \Exception(sprintf("The %s binary is not executable. The htmlTo%s library cannot be used.", $executable, strtoupper($component)));
-			}
-		}
-	}
-	
-	/**
-	 * Checks the architecture of the server this plugin is being run on
-	 *
-	 * @return bool $is64Bit
-	 */
-	private function is64Bit()
-	{
-		return (PHP_INT_SIZE === 8);
-	}
-	
-	/**
-	 * Generates the path to a binary based on the vendor directory existance
-	 *
-	 * @param string $binaryBasen
-	 * @return string $path
-	 */
-	private function getBinaryPath($binaryBasename)
-	{
-		$pluginVendorDir = plugins_path('luketowers/snappypdf/vendor/');
-		$binPath = "bin/$binaryBasename";
-		
-		// Potential sources where the binary could be stored
-		$potentialSources = [
-			$pluginVendorDir . $binPath,
-			$pluginVendorDir . "$binaryBasename/$binPath",
-			base_path('vendor/' . $binPath),
-			base_path("$binaryBasename/$binPath"),
-		];
-		
-		foreach ($potentialSources as $path) {
-			if (is_executable($path)) {
-				return $path;
-			}
-		}
-		
-		throw new \Exception("The $binaryBasename binary could not be detected anywhere. Please manually specify the path to this binary through the SNAPPY_TYPE_BINARY environment variables or by setting the paths in config/luketowers/snappypdf/config.php");
-	}
+     * Checks the configuration for the plugin
+     */
+    public function validateConfig()
+    {
+        $checkComponents = [
+            'pdf' => [
+                'binary_x64' => 'wkhtmltopdf-amd64',
+                'binary_x32' => 'wkhtmltopdf-i386',
+            ],
+            'image' => [
+                'binary_x64' => 'wkhtmltoimage-amd64',
+                'binary_x32' => 'wkhtmltoimage-i386',
+            ]
+        ];
+        
+        foreach ($checkComponents as $component => $options) {
+            if (!Config::get("snappy.$component.enabled")) {
+                continue;
+            }
+            
+            if (empty(env('SNAPPY_' . strtoupper($component) . '_BINARY'))) {
+                $binaryPathConfigKey = 'snappy.' . $component . '.binary';
+                $binaryBasename = pathinfo(Config::get($binaryPathConfigKey), PATHINFO_BASENAME);
+                if (!file_exists(Config::get($binaryPathConfigKey))) {
+                    Config::set($binaryPathConfigKey, $this->getBinaryPath($binaryBasename));
+                }
+                
+                if (!$this->is64Bit() && ($binaryBasename === $options['binary_x64'])) {
+                    Config::set($binaryPathConfigKey, $this->getBinaryPath($options['binary_x32']));
+                }
+            }
+            
+            $executable = Config::get($binaryPathConfigKey); 
+            if (!is_executable($executable)) {
+                throw new \Exception(sprintf("The %s binary is not executable. The htmlTo%s library cannot be used.", $executable, strtoupper($component)));
+            }
+        }
+    }
+    
+    /**
+     * Checks the architecture of the server this plugin is being run on
+     *
+     * @return bool $is64Bit
+     */
+    private function is64Bit()
+    {
+        return (PHP_INT_SIZE === 8);
+    }
+    
+    /**
+     * Generates the path to a binary based on the vendor directory existance
+     *
+     * @param string $binaryBasen
+     * @return string $path
+     */
+    private function getBinaryPath($binaryBasename)
+    {
+        $pluginVendorDir = plugins_path('luketowers/snappypdf/vendor/');
+        $binPath = "bin/$binaryBasename";
+        
+        // Potential sources where the binary could be stored
+        $potentialSources = [
+            $pluginVendorDir . $binPath,
+            $pluginVendorDir . "$binaryBasename/$binPath",
+            base_path('vendor/' . $binPath),
+            base_path("$binaryBasename/$binPath"),
+        ];
+        
+        foreach ($potentialSources as $path) {
+            if (is_executable($path)) {
+                return $path;
+            }
+        }
+        
+        throw new \Exception("The $binaryBasename binary could not be detected anywhere. Please manually specify the path to this binary through the SNAPPY_TYPE_BINARY environment variables or by setting the paths in config/luketowers/snappypdf/config.php");
+    }
 }
