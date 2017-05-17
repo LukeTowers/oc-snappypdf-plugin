a6b612f2df4a277c92803d1bf5ef1d283e5f305b
Luke Towers <luke@luketowers.ca>
2017-05-16 19:57:29 -0600

    Attempt to make the binaries executable before freaking out

--- Plugin.php
+++ Plugin.php
@@ -25,19 +25,19 @@ class Plugin extends PluginBase
             'homepage'    => 'https://github.com/LukeTowers/oc-snappypdf-plugin',
         ];
     }
-    
+
     /**
      * Runs right before the request route
      */
     public function boot()
-    {    
+    {
         // Setup required packages
         $this->bootPackages();
-        
+
         // Overwrite config values if necessary
         $this->validateConfig();
     }
-    
+
     /**
      * Boots (configures and registers) any packages found within this plugin's packages.load configuration value
      *
@@ -48,27 +48,27 @@ class Plugin extends PluginBase
     {
         // Get the namespace of the current plugin to use in accessing the Config of the plugin
         $pluginNamespace = str_replace('\\', '.', strtolower(__NAMESPACE__));
-        
+
         // Instantiate the AliasLoader for any aliases that will be loaded
         $aliasLoader = AliasLoader::getInstance();
-        
+
         // Get the packages to boot
         $packages = Config::get($pluginNamespace . '::packages');
-        
+
         // Boot each package
         foreach ($packages as $name => $options) {
             // Setup the configuration for the package, pulling from this plugin's config
             if (!empty($options['config']) && !empty($options['config_namespace'])) {
                 Config::set($options['config_namespace'], $options['config']);
             }
-            
+
             // Register any Service Providers for the package
             if (!empty($options['providers'])) {
                 foreach ($options['providers'] as $provider) {
                     App::register($provider);
                 }
             }
-            
+
             // Register any Aliases for the package
             if (!empty($options['aliases'])) {
                 foreach ($options['aliases'] as $alias => $path) {
@@ -77,7 +77,7 @@ class Plugin extends PluginBase
             }
         }
     }
-    
+
     /**
      * Checks the configuration for the plugin
      */
@@ -93,31 +93,31 @@ class Plugin extends PluginBase
                 'binary_x32' => 'wkhtmltoimage-i386',
             ]
         ];
-        
+
         foreach ($checkComponents as $component => $options) {
             if (!Config::get("snappy.$component.enabled")) {
                 continue;
             }
-            
+
             if (empty(env('SNAPPY_' . strtoupper($component) . '_BINARY'))) {
                 $binaryPathConfigKey = 'snappy.' . $component . '.binary';
                 $binaryBasename = pathinfo(Config::get($binaryPathConfigKey), PATHINFO_BASENAME);
                 if (!file_exists(Config::get($binaryPathConfigKey))) {
                     Config::set($binaryPathConfigKey, $this->getBinaryPath($binaryBasename));
                 }
-                
+
                 if (!$this->is64Bit() && ($binaryBasename === $options['binary_x64'])) {
                     Config::set($binaryPathConfigKey, $this->getBinaryPath($options['binary_x32']));
                 }
             }
-            
-            $executable = Config::get($binaryPathConfigKey); 
-            if (!is_executable($executable)) {
+
+            $executable = Config::get($binaryPathConfigKey);
+            if (!$this->checkPathExecutable($executable)) {
                 throw new \Exception(sprintf("The %s binary is not executable. The htmlTo%s library cannot be used.", $executable, strtoupper($component)));
             }
         }
     }
-    
+
     /**
      * Checks the architecture of the server this plugin is being run on
      *
@@ -127,7 +127,7 @@ class Plugin extends PluginBase
     {
         return (PHP_INT_SIZE === 8);
     }
-    
+
     /**
      * Generates the path to a binary based on the vendor directory existance
      *
@@ -138,7 +138,7 @@ class Plugin extends PluginBase
     {
         $pluginVendorDir = plugins_path('luketowers/snappypdf/vendor/');
         $binPath = "bin/$binaryBasename";
-        
+
         // Potential sources where the binary could be stored
         $potentialSources = [
             $pluginVendorDir . $binPath,
@@ -146,13 +146,30 @@ class Plugin extends PluginBase
             base_path("vendor/$binPath"),
             base_path("vendor/$binaryBasename/$binPath"),
         ];
-        
+
         foreach ($potentialSources as $path) {
-            if (is_executable($path)) {
+            if ($this->checkPathExecutable($path)) {
                 return $path;
             }
         }
-        
+
         throw new \Exception("The $binaryBasename binary could not be detected anywhere. Please manually specify the path to this binary through the SNAPPY_TYPE_BINARY environment variables or by setting the paths in config/luketowers/snappypdf/config.php");
+    }
+
+    /**
+     * Checks if the provided path is executable and attempts to make it executable if not
+     *
+     * @param string $path The path to check
+     * @return bool
+     */
+    private function checkPathExecutable($path)
+    {
+        if (!is_executable($path) && file_exists($path)) {
+            // Attempt to make the path executable and clear the stat cache for the path for checking again
+            // TODO: Look into using fileperms() to dynamically reassigning permissions to only add +x to the user permission
+            chmod($path, 0764);
+            clearstatcache(true, $path)
+        }
+        return is_executable($path);
     }
 }
