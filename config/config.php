e53f114ae3c91dccf320b3ce4e392daf98abb833
Luke Towers <luke@luketowers.ca>
2017-07-08 10:08:39 -0600

    Github prefers spaces :/

--- config/config.php
+++ config/config.php
@@ -4,29 +4,29 @@
             'providers' => [
                 '\Barryvdh\Snappy\ServiceProvider',
             ],
-            
+
             'aliases' => [
                 'SnappyPDF'   => '\Barryvdh\Snappy\Facades\SnappyPdf',
                 'SnappyImage' => '\Barryvdh\Snappy\Facades\SnappyImage',
             ],
-            
+
             'config_namespace' => 'snappy',
-            
+
             'config' => [
                 'pdf' => array(
-			        'enabled' => true,
-			        'binary'  => env('SNAPPY_PDF_BINARY', plugins_path('luketowers/snappypdf/vendor/bin/wkhtmltopdf-amd64')),
-			        'timeout' => false,
-			        'options' => array(),
-			        'env'     => array(),
-			    ),
-			    'image' => array(
-			        'enabled' => true,
-			        'binary'  => env('SNAPPY_IMAGE_BINARY', plugins_path('luketowers/snappypdf/vendor/bin/wkhtmltoimage-amd64')),
-			        'timeout' => false,
-			        'options' => array(),
-			        'env'     => array(),
-			    ),
+                    'enabled' => true,
+                    'binary'  => env('SNAPPY_PDF_BINARY', plugins_path('luketowers/snappypdf/vendor/bin/wkhtmltopdf-amd64')),
+                    'timeout' => false,
+                    'options' => array(),
+                    'env'     => array(),
+                ),
+                'image' => array(
+                    'enabled' => true,
+                    'binary'  => env('SNAPPY_IMAGE_BINARY', plugins_path('luketowers/snappypdf/vendor/bin/wkhtmltoimage-amd64')),
+                    'timeout' => false,
+                    'options' => array(),
+                    'env'     => array(),
+                ),
             ],
         ],
     ],
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 return [
-	'plugin'      => [
-		'name'        => 'SnappyPDF',
-		'description' => 'Integrates the barryvdh/laravel-snappy PDF / Image generation libraries into OctoberCMS.',
-	],
+    'plugin'      => [
+        'name'        => 'SnappyPDF',
+        'description' => 'Integrates the barryvdh/laravel-snappy PDF / Image generation libraries into OctoberCMS.',
+    ],
 ];
